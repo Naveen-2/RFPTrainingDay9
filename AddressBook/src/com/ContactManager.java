425a27bb19f52b03ea7ae135027d34514735fda2
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-23 22:30:53 +0530

    Now contacts can be edited based on first name

--- AddressBook/src/com/ContactManager.java
+++ AddressBook/src/com/ContactManager.java
@@ -23,4 +23,26 @@ public class ContactManager {
 
     }
 
+    public void editByName(String name) {
+        for (Contact contact : contactList) {
+            if (name.equalsIgnoreCase(contact.firstName)) {
+                editContact(contact);
+            } else {
+                System.out.println("Contact nor found");
+            }
+        }
+    }
+
+    public void editContact(Contact contact) {
+        contact.setFirstName(ScannerUtil.getString("Enter new First name: "));
+        contact.setLastName(ScannerUtil.getString("Enter new Last name: "));
+        contact.setPhoneNumber(ScannerUtil.getLong("Enter new Phone number: "));
+        contact.setEmail(ScannerUtil.getString("Enter new Email: "));
+        contact.setAddress(ScannerUtil.getString("Enter new Address: "));
+        contact.setCity(ScannerUtil.getString("Enter new City: "));
+        contact.setState(ScannerUtil.getString("Enter new State: "));
+        contact.setZip(ScannerUtil.getInt("Enter new Zip code: "));
+    }
+
+
 }
--- AddressBook/src/com/UserMenu.java
+++ AddressBook/src/com/UserMenu.java
@@ -6,20 +6,24 @@ public class UserMenu {
     Scanner input = new Scanner(System.in);
 
     public int showMainMenu() {
-        System.out.println("Main Menu:\n 1.Add Contact");
+        System.out.println("Main Menu:\n 1.Add Contact \n 2.Edit Contact");
         System.out.print("Enter your choice: ");
         return input.nextInt();
     }
 
     public void handleUserSelection() {
         ContactManager contactManager = new ContactManager();
-        do {
+        loop : do {
             int choice = showMainMenu();
-            if (choice == 1) {
-                contactManager.addNewContact();
-                System.out.println("Contact added to address book successfully.\n");
-            } else {
-                break;
+            switch (choice) {
+                case 1 -> {
+                    contactManager.addNewContact();
+                    System.out.println("Contact added to address book successfully.\n");
+                }
+                case 2 -> contactManager.editByName(ScannerUtil.getString("Enter contact First name to edit: "));
+                default -> {
+                    break loop;
+                }
             }
         } while (true);
     }
