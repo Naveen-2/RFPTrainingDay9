0a21340afe7286c4a6aa2a5dbb3c7e3495af2f7d
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-23 13:40:32 +0530

    Address book created and one contact updated

--- AddressBook/src/com/Main.java
+++ AddressBook/src/com/Main.java
@@ -3,7 +3,10 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Welcome message for UC1
         System.out.println("Welcome to Address Book Program.");
+        //New contact created in address book
+        AddressBook contact1 = new AddressBook("Test","Name","new road","svks","626123","1234567890","abc@def.com");
+        //System.out.println(contact1.address);
     }
 }
