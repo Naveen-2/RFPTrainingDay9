8e365150b71913f82bf6e31668caf9845c712da4
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-23 22:11:10 +0530

    UC2 done to add contacts to addressbook in contactsList

--- AddressBook/src/com/Main.java
+++ AddressBook/src/com/Main.java
@@ -3,10 +3,8 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        // Welcome message for UC1
-        System.out.println("Welcome to Address Book Program.");
-        //New contact created in address book
-        AddressBook contact1 = new AddressBook("Test","Name","new road","svks","626123","1234567890","abc@def.com");
-        //System.out.println(contact1.address);
+        System.out.println("Welcome to Address Book Program");
+        UserMenu userMenu = new UserMenu();
+        userMenu.handleUserSelection();
     }
 }
